9a9930c32ed5605f05ad1b82f6fdbdbdbe78b9f5
nowahgit <nimroterikson6@gmail.com>
2026-04-03 18:15:32 +0700

    add 100 user on db seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,12 +4,9 @@ namespace Database\Seeders;
 
 use App\Models\Admin;
 use App\Models\User;
-use App\Models\Berkas;
-use App\Models\Seleksi;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -33,7 +30,7 @@ class DatabaseSeeder extends Seeder
             );
 
             // 2. Create the corresponding admins record
-            $admin = Admin::updateOrCreate(
+            Admin::updateOrCreate(
                 ['user_id' => $adminUser->id],
                 [
                     'nama_panitia' => 'Super Admin',
@@ -41,79 +38,17 @@ class DatabaseSeeder extends Seeder
                 ]
             );
 
-            // 3. Create Demo Users for Testing
-            $faker = Faker::create('id_ID');
-            $statuses = ['MENUNGGU', 'LULUS', 'TIDAK_LULUS'];
-            $berkasStatuses = ['MENUNGGU', 'VALID', 'DITOLAK'];
-
-            for ($i = 1; $i <= 10; $i++) {
+            // 3. Create 100 blank PENDAFTAR users (simulating fresh registrations)
+            for ($i = 1; $i <= 100; $i++) {
                 $username = 'siswa' . $i;
-                
-                // Fetch existing or prepare new data
-                $existingUser = User::where('username', $username)->first();
-                $nisn = $existingUser->nisn_pendaftar ?? $faker->unique()->numerify('##########');
-                $nama = $existingUser->nama_pendaftar ?? $faker->name;
-                $alamat = $existingUser->alamat_pendaftar ?? $faker->address;
-                $agama = $existingUser->agama ?? $faker->randomElement(['Islam', 'Kristen', 'Katolik', 'Hindu', 'Buddha']);
 
-                // Create/Update User (PENDAFTAR)
-                $user = User::updateOrCreate(
+                User::updateOrCreate(
                     ['username' => $username],
                     [
-                        'password' => Hash::make('siswa123'),
-                        'role'     => 'PENDAFTAR',
-                        'email'    => $username . '@gmail.com',
-                        'nomor_pendaftaran' => $existingUser->nomor_pendaftaran ?? 'PPDB' . date('Y') . sprintf('%04d', $i),
-                        'no_hp' => $existingUser->no_hp ?? '08' . $faker->numerify('##########'),
-                        'jenis_kelamin' => $existingUser->jenis_kelamin ?? $faker->randomElement(['LAKI-LAKI', 'PEREMPUAN']),
-                        'nisn_pendaftar' => $nisn,
-                        'nama_pendaftar' => $nama,
-                        'tanggallahir_pendaftar' => $existingUser->tanggallahir_pendaftar ?? $faker->dateTimeBetween('-16 years', '-14 years')->format('Y-m-d'),
-                        'alamat_pendaftar' => $alamat,
-                        'agama' => $agama,
-                        'nama_ortu' => $existingUser->nama_ortu ?? $faker->name,
-                        'pekerjaan_ortu' => $existingUser->pekerjaan_ortu ?? $faker->jobTitle,
-                        'no_hp_ortu' => $existingUser->no_hp_ortu ?? '08' . $faker->numerify('##########'),
-                        'alamat_ortu' => $alamat,
-                        'nilai_smt1' => $existingUser->nilai_smt1 ?? $faker->randomFloat(1, 75, 100),
-                        'nilai_smt2' => $existingUser->nilai_smt2 ?? $faker->randomFloat(1, 75, 100),
-                        'nilai_smt3' => $existingUser->nilai_smt3 ?? $faker->randomFloat(1, 75, 100),
-                        'nilai_smt4' => $existingUser->nilai_smt4 ?? $faker->randomFloat(1, 75, 100),
-                        'nilai_smt5' => $existingUser->nilai_smt5 ?? $faker->randomFloat(1, 75, 100),
-                    ]
-                );
-
-                // Create/Update Berkas for testing
-                Berkas::updateOrCreate(
-                    ['user_id' => $user->id],
-                    [
-                        'nisn_pendaftar' => $nisn,
-                        'nama_pendaftar' => $nama,
-                        'tanggallahir_pendaftar' => $user->tanggallahir_pendaftar,
-                        'alamat_pendaftar' => $alamat,
-                        'agama' => $agama,
-                        'nama_ortu' => $user->nama_ortu,
-                        'pekerjaan_ortu' => $user->pekerjaan_ortu,
-                        'no_hp_ortu' => $user->no_hp_ortu,
-                        'alamat_ortu' => $alamat,
-                        'status_validasi' => $existingUser ? ($user->berkas->status_validasi ?? 'MENUNGGU') : $faker->randomElement($berkasStatuses),
-                    ]
-                );
-
-                // Create/Update Seleksi for testing
-                Seleksi::updateOrCreate(
-                    ['user_id' => $user->id],
-                    [
-                        'id_panitia' => $admin->id_panitia,
-                        'nama_seleksi' => 'Seleksi PPDB Gelombang 1',
-                        'nilai_smt1' => $user->nilai_smt1,
-                        'nilai_smt2' => $user->nilai_smt2,
-                        'nilai_smt3' => $user->nilai_smt3,
-                        'nilai_smt4' => $user->nilai_smt4,
-                        'nilai_smt5' => $user->nilai_smt5,
-                        'status_seleksi' => $existingUser ? ($user->seleksis->first()->status_seleksi ?? 'MENUNGGU') : $faker->randomElement($statuses),
-                        'waktu_seleksi' => now(),
-                        'is_archived' => false,
+                        'password'          => Hash::make('siswa123'),
+                        'role'              => 'PENDAFTAR',
+                        'email'             => $username . '@gmail.com',
+                        'nomor_pendaftaran' => 'PPDB' . date('Y') . sprintf('%04d', $i),
                     ]
                 );
             }
